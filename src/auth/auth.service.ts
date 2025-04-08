1344389066fd6b1d32a4d56251ee1d31d512bffd
sid-sg <sengupta.sid03@gmail.com>
2025-04-09 04:34:46 +0530

    added auth dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
 
 @Injectable({})
 export class AuthService{
     constructor(private prisma: PrismaService){}
 
-    signup(){
-        return 'user signed up';
+    signup(dto: AuthDto){
+        return {
+            'msg': `user signed up`
+        };
     }
 
-    signin(){
-        return 'user signed in';
+    signin(dto: AuthDto){
+        return {
+            'msg': 'user signed in'
+        };
     }
 }
